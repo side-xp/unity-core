9b43d1c57983cd49c5f71c76e43d64710b097ebb
Hubert Grossin <hubert.grossin@gmail.com>
2026-06-22 22:16:31 +0200

    chore: migrate to Unity 6.5

--- Demos/Runtime/ItemSO.cs
+++ Demos/Runtime/ItemSO.cs
@@ -22,10 +22,17 @@ namespace SideXP.Core.Demos
 #if UNITY_EDITOR
         // Illustrates the usage of the OpenAsset callback. See ItemSOEditorWindow script from editor demos to learn more about handlers.
         [UnityEditor.Callbacks.OnOpenAsset]
+#if UNITY_6000_5_OR_NEWER
+        private static bool OpenAsset(EntityId entityId, int line)
+        {
+            return RuntimeObjectUtility.TryOpenAsset(entityId, line);
+        }
+#else
         private static bool OpenAsset(int instanceId, int line)
         {
             return RuntimeObjectUtility.TryOpenAsset(instanceId, line);
         }
+#endif
 #endif
 
     }
