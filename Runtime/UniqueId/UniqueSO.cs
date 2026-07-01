10f8aceef5709eced885dcf7e0b5657293fd3101
Hubert Grossin <hubert.grossin@gmail.com>
2026-07-01 23:04:13 +0200

    fix: fixed UniqueId feature behavior

--- Runtime/UniqueId/UniqueSO.cs
+++ Runtime/UniqueId/UniqueSO.cs
@@ -4,7 +4,7 @@ namespace SideXP.Core
 {
 
     /// <summary>
-    /// Represents an asst with a unique identifier.
+    /// Represents an asset with a unique identifier.
     /// </summary>
     [HelpURL(Constants.BaseHelpUrl + "/api/SideXP.Core/" + nameof(UniqueSO))]
     public class UniqueSO : ScriptableObject
