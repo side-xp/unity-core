d5ca793f7479b7b73fe166c3be7b3d78ca0353ee
Hubert Grossin <hubert.grossin@gmail.com>
2026-09-20 02:08:58 +0200

    fix: added `NotEditable` option for subassets

--- Runtime/Subassets/SubassetsListOptionsAttribute.cs
+++ Runtime/Subassets/SubassetsListOptionsAttribute.cs
@@ -15,6 +15,15 @@ namespace SideXP.Core
         public bool DisallowRename { get; set; } = false;
 
         /// <summary>
+        /// By default, the properties of the subassets can be edited from the list itself.<br/>
+        /// If enabled, each item is displayed as a disabled object field instead. The subassets can still be selected and edited from the
+        /// inspector by double-clicking on that field.
+        /// </summary>
+        /// <remarks>This is especially useful if the subassets have <see cref="SubassetsList{T}"/> properties themselves, since nesting
+        /// these lists makes the inspector compute wrong GUI heights.</remarks>
+        public bool NotEditable { get; set; } = false;
+
+        /// <summary>
         /// By default, the list is allowed to contain several instances of the same subasset type.<br/>
         /// If enabled, types already existing in the list will be disabled in the inspector.
         /// </summary>
